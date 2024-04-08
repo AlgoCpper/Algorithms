9d0e213f0fa03f93074aa32c84353cdcbc6e5c16
JANGJAGUAR <jangjaguar@gmail.com>
2024-04-08 23:42:41 +0900

    feat: 1715.cpp

---
+++ week2/240408_BOJ_1715/1715.cpp
@@ -0,0 +1,34 @@
+//G4_1715
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int n, input;
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> input;
+        pq.push(input);
+    }
+
+    int cnt=0;
+    while(pq.size()>1){
+        int a, b;
+        a = pq.top();
+        pq.pop();
+        b = pq.top();
+        pq.pop();
+        pq.push(a+b);
+        cnt+=(a+b);
+        cout << (a+b);
+    }
+    cout << cnt;
+    
+}
