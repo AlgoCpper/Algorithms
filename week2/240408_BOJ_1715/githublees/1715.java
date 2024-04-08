50b61e3d1d7cec76ba0c6d03b65505e2277618fb
githublees <leeho_@naver.com>
2024-04-08 10:34:17 +0900

    feat: 1715.java

---
+++ week2/240408_BOJ_1715/githublees/1715.java
@@ -0,0 +1,35 @@
+package backjoon;
+
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class BoJ_1715_카드_정렬하기 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int answer = 0;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for(int i=0; i<N; i++) {
+			pq.offer(Integer.parseInt(br.readLine()));
+		}
+		
+		while(true) {
+			
+			if(pq.size() == 1) break;
+			
+			int A = pq.poll();
+			int B = pq.poll();
+			int C = A + B;
+			
+			answer += C;
+			
+			pq.offer(C);
+		}
+		
+		System.out.println(answer);
+	}
+}
